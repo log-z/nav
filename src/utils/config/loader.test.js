75706bcc255b294c1cfb1b601726818ece6524e3
log <535065234@qq.com>
2023-03-05 01:27:38 +0800

    使用Vitest替换Jest，完善配置加载单元测试

--- src/utils/config/loader.test.js
+++ src/utils/config/loader.test.js
@@ -1,9 +1,12 @@
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { afterAll, beforeAll, afterEach, describe, expect, test } from 'vitest'
 import { configLoader } from '.';
 import { subscribeLoader as themeSubscribeLoader } from './loader-v2-theme'
 
 describe('Test config loader', () => {
-  test('simple loader', async () => {
-    const conf = await configLoader({
+  test('simple loader V1', async () => {
+    const confRaw = {
       "name": "test",
       "description": "test",
       "searchEngine": {
@@ -27,20 +30,22 @@ describe('Test config loader', () => {
           }]
         }]
       }
-    });
+    }
+
+    const conf = await configLoader(confRaw);
     expect(conf).not.toBeNull()
   })
 
   test('simple loader V2', async () => {
-    const conf = await configLoader({
+    const confRaw = {
       "version": "2",
       "name": "test",
       "description": "test",
       "theme": {
         "subscribe": {
-          "my": "theme-my.json"
+          "nav": "config:nav-theme-preset.json"
         },
-        "active": "my.red",
+        "active": "nav.red",
         "custom": {
           "light@primary.color": "#000",
           "light@base.background.color": "#000",
@@ -51,13 +56,7 @@ describe('Test config loader', () => {
       "search": {
         "placeholder": "👴 来点什么？",
         "engine": {
-          "active": ["baidu", "google", "bing", "wikipedia"],
-          "mirror": {
-            "google": {
-              "baseUrl": null,
-              "baseCompleteUrl": null
-            }
-          }
+          "active": ["baidu", "google", "bing", "wikipedia"]
         }
       },
       "favorites": {
@@ -77,30 +76,59 @@ describe('Test config loader', () => {
           }]
         }]
       }
-    });
-    expect(conf).not.toBeNull()
+    }
+
+    const conf = await configLoader(confRaw)
+    expect(conf).toMatchObject(confRaw)
   })
 
   test('simple loader V2 theme subscribe', async () => {
-    const conf = await themeSubscribeLoader([
-      {
-        "name": "red",
-        "custom": {
-          "light@primary.color": "#eb7366",
-          "light@base.background.color": "#fff8f8",
-          "light@footer.background.color": "#fdefef",
-          "light@highlight.background.color": "#fddbd6"
-        }
-      }, {
-        "name": "green",
-        "custom": {
-          "light@primary.color": "#92bf9a",
-          "light@base.background.color": "#fdffe0",
-          "light@footer.background.color": "#92bf9a",
-          "light@highlight.background.color": "#f7f9d6"
-        }
-      }
-    ]);
-    expect(conf).not.toBeNull()
+    const themeSubscribe = await themeSubscribeLoader(themeSubscribeData);
+    expect(themeSubscribe).toMatchObject(themeSubscribeData)
   })
 })
+
+
+/*
+ 请求模拟
+ */
+
+const themeSubscribeData = [
+  {
+    "name": "default",
+    "custom": {
+      "light@primary.color": "#eb7366",
+      "light@base.background.color": "#fff8f8",
+      "light@footer.background.color": "#fdefef",
+      "light@highlight.background.color": "#fddbd6"
+    }
+  }, {
+    "name": "red",
+    "custom": {
+      "light@primary.color": "#eb7366",
+      "light@base.background.color": "#fff8f8",
+      "light@footer.background.color": "#fdefef",
+      "light@highlight.background.color": "#fddbd6"
+    }
+  }, {
+    "name": "green",
+    "custom": {
+      "light@primary.color": "#92bf9a",
+      "light@base.background.color": "#fdffe0",
+      "light@footer.background.color": "#92bf9a",
+      "light@highlight.background.color": "#f7f9d6"
+    }
+  }
+]
+
+const restHandlers = [
+  rest.get(import.meta.env.VITE_DEFAULT_THEME_URL, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(themeSubscribeData))
+  }),
+]
+
+const server = setupServer(...restHandlers)
+
+beforeAll(() => server.listen({}))
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
